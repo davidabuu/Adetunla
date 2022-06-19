1a5521a2f12a0b767a1ec7a4b2e22d97bf4d4ef5
Abu David <davidabu80@gmail.com>
2022-06-19 22:20:44 +0100

    Updates

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { IntroStyle } from '../../StyledComponents/Styled';
-import Aos from 'aos';
-import 'aos/dist/aos.css'
 const Intro = () => {
   return (
     <IntroStyle>
